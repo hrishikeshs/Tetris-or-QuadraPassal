2d69e5532ff70fa22d970846772cb6e45ffb57ba
Hrishikesh S <hrish2006@gmail.com>
2013-03-28 00:13:23 +0530

    Update ZigzagRight.java

--- ZigzagRight.java
+++ ZigzagRight.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 
 public class ZigzagRight extends GShape{
 
-  /* This class represents the 'S tetrimono' . It has a rotate() method that flips the
+	/* This class represents the 'S tetrimono' . It has a rotate() method that flips the
 	 * tetrimono appropriately. It inherits the 'rotated' field from GShape to keep
 	 * track of the current orientation.
 	 * Again, all positions are relative to the origin (0,0) assumed to be at the top left corner. 
@@ -11,41 +11,40 @@ public class ZigzagRight extends GShape{
 	
 	/*Private instance variables */
 	
-		private Color  color  = Color.orange;
-		private Brick first   = new Brick(color);
-		private Brick second  = new Brick(color);
-		private Brick third   = new Brick(color);
-		private Brick fourth  = new Brick(color);
+		private static final Color  color  = Color.orange;
+		
 				 
 		
 		public ZigzagRight() {
 		
-			rotated  = 1;
-			add(first.brick(),0,SIDE);
-			add(second.brick(),SIDE,SIDE);
-			add(third.brick(),SIDE,0);
-			add(fourth.brick(),2*SIDE,0);
+			super(color);
+			orientation  = 1;
+			type = 5;
+			add(first,0,SIDE);
+			add(second,SIDE,SIDE);
+			add(third,SIDE,0);
+			add(fourth,2*SIDE,0);
 		}
 	
 	
-		public void Rotate() {
+		public void rotateShape() {
 			
-			if(rotated == 1) {
+			if(orientation == 1) {
 
-				add(first.brick(),0,0);
-				add(second.brick(),0,SIDE);
-				add(third.brick(),SIDE,SIDE);
-				add(fourth.brick(),SIDE,2*SIDE);
+				add(first,0,0);
+				add(second,0,SIDE);
+				add(third,SIDE,SIDE);
+				add(fourth,SIDE,2*SIDE);
 		
-				rotated = 2;
+				orientation = 2;
 			}		
 			else {
-				add(first.brick(),0,SIDE);
-				add(second.brick(),SIDE,SIDE);
-				add(third.brick(),SIDE,0);
-				add(fourth.brick(),2*SIDE,0);
+				add(first,0,SIDE);
+				add(second,SIDE,SIDE);
+				add(third,SIDE,0);
+				add(fourth,2*SIDE,0);
 						
-				rotated = 1;
+				orientation = 1;
 			}
 	}		
 		
